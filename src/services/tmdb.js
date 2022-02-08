b00eb7868cc56df52c838399c62f5b958d1c32ac
Bruno Azevedo <onurbazv@gmail.com>
2022-02-08 09:10:51 -0300

    added tailwind

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -1,9 +1,10 @@
-import { API_KEY } from '../constants/config'
-
-export const fetchResults = (query, page=null) => {
-    return fetch(
+// import { API_KEY } from '../constants/config'
+const API_KEY = "7dd4eac08ee75e1288a33d6d52741be1"
+export const fetchResults = async (query, page=null) => {
+    const req = await fetch(
         `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}${page === null ? "" : `&page=${page}`}`
         )
-        .then(r => r.json())
-        .then(r => r)
+    const data = await req.json()
+    console.log(data)
+    return data
 }
