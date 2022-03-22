d8f6d32fcd406d9862e83413af6df4ab742067a2
Bruno Azevedo <onurbazv@gmail.com>
2022-03-22 19:07:09 -0300

    fixed Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,15 @@ const Footer = ({language}) => {
                 <div className="mr-auto">
                     © 2022 Bruno Azevedo
                 </div>
-                <a className="flex items-center" href="https://www.themoviedb.org/">
+                <div className="flex items-center">
                     <p className="mr-4">{STRINGS[language]['ATTRIBUTION']} TMDB API & JustWatch</p>
-                    <img src="./img/tmdb-logo-full.svg" alt="TheMovieDB logo" className="w-12 mr-2"/>
-                    <img src="./img/just-watch-logo-2.png" alt="Just Watch logo" className="w-10 rounded"/>
-                </a>
+                    <a href="https://www.themoviedb.org/">
+                        <img src="./img/tmdb-logo-full.svg" alt="TheMovieDB logo" className="w-12 mr-2"/>
+                    </a>
+                    <a href="https://www.justwatch.com/">
+                        <img src="./img/jw-logo.png" alt="Just Watch logo" className="w-10 rounded"/>
+                    </a>
+                </div>
             </div>
         </footer>
     )
